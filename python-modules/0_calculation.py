174cd2d56491d537ff840ee10f918b853fb23779
natautomation <147779652+natautomation@users.noreply.github.com>
2023-10-22 19:51:50 -0700

    Create 0_calculation.py

---
+++ python-modules/0_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    # imports
+    from calculation import add, sub, mul, div
+
+    a = 4  # passing parameter one to function
+    b = 2  # passing parameter two to function
+
+    # Ading Print Statement with parameters.
+    print(add(a, b))
+    print(sub(a, b))
+    print(mul(a, b))
+    print(div(a, b))
