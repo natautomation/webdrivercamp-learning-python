5a5179c6e57921f65d128dbae5ec67399011bf5d
natautomation <147779652+natautomation@users.noreply.github.com>
2023-10-22 20:12:34 -0700

    Create 2_modify_numbers.py

---
+++ python_beginning/2_modify_numbers.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+level = 3.141592653589793238
+print(f"Learning Python is fun\"' - {round(level,2)} %")
+print("Learning Python is fun\"' - {} %".format(round(level,2)))
+print("Learning Python is fun\"' - %s" % round(level,2), "%")
