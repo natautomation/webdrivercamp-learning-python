9473bb9c9748cc2d9fc4ffbdfc5f9b17cbea59e3
natautomation <147779652+natautomation@users.noreply.github.com>
2023-10-22 20:21:52 -0700

    Create text.py

---
+++ python_beginning/text.py
@@ -0,0 +1,152 @@
+#0x1 Python - Beginning!
+#Requirements for Python scripts
+#Allowed editors are Vim, Vi, Emacs
+
+#The first line of all your files should have Python shebang (like this #!/usr/bin/python3)
+
+#You are not allowed to cast anything to string in the tasks
+
+#You are not allowed to use any loops or conditional statements.
+
+#A README.md file at the root of the repo, containing a description of the repository
+
+vA README.md file, at the root of the folder of this project, is mandatory
+
+#Your code should use the pycodestyle
+
+#You should follow best practices in writing you code - python style guides
+
+#All your files must be executable
+
+#All the files in this project should be in GitHub Repository: webdrivercamp-learning-python/python_beginning
+
+ 
+
+#:blue_book: Exercise
+#0. Print
+#python docs - tutorial 
+
+#Write a Python script that prints exactly Learning Python is fun"'
+
+
+
+ #$ ./0_simple_print.py 
+#Learning Python is fun"'
+ #$ 
+File: 0_simple_print.py
+
+#1. Add some numbers
+#string format
+
+#Copy the file from task #0
+
+#Rename it to 1_adding_some_numbers.py
+
+#In that file before printing create a variable and assign number 100 to it
+
+#Now modify the print to use this variable to get: Learning Python is fun"' - 100 %
+
+#Repeat the print 3 time using 3 different string formats from newest to oldest
+
+
+
+ # $ ./1_adding_some_numbers.py
+#Learning Python is fun"' - 100 %
+#Learning Python is fun"' - 100 %
+#Learning Python is fun"' - 100 %
+ v$ 
+#File: 1_adding_some_numbers.py
+
+#2. Print a float
+#Copy the file from task #1
+
+#Rename it to 2_modify_numbers.py
+
+#Change the number 100 to 3.141592653589793238
+
+#Now modify the prints to get the float number printed with only 2 digits followed
+
+
+
+ #$ ./2_modify_numbers.py
+#Learning Python is fun"' - 3.14 %
+#Learning Python is fun"' - 3.14 %
+#Learning Python is fun"' - 3.14 %
+# $ 
+#File: 2_modify_numbers.py
+
+#3. PrintPrintPrint
+#Create a variable and assign “Yes!“ to it
+
+#Print the variable 4 times
+
+
+
+ #$ ./3_pritnprint.py
+#Yes!Yes!Yes!Yes!
+# $ 
+#File: 3_printprint.py
+
+#4. Concat strings
+#string concatenation
+
+#Create a variable and assign “Webdriver” to it
+
+#Create another variable and assign “Camp“ to it
+
+#Create a third variable and assign two strings above concatenated
+
+#Now print using the third variable only
+
+
+
+ #$ ./4_two_str_print.py 
+#Welcome to Webdriver Camp!
+ #$ 
+#File: 4_two_str_print.py
+#5. Slice that, please 
+#slicing strings
+
+#Create one variable and assign Webdriver Awesome Camp to it
+
+#Print every word of this string on a new line
+
+
+
+ #$ ./5_sep_str_print.py 
+#Webdriver
+#Awesome
+#Camp
+# $ 
+#File: 5_slice_that.py
+
+#6. Print only what is needed
+#Create a variable and assign this to it:
+
+#"There should be one-- and preferably only one --obvious way to do it. lthough that way may not be obvious at first unless you're Dutch.”
+
+#Modify the variable to get preferably only one way unless
+
+#Print the variable
+
+#Note: You should have only one variable
+
+
+
+ #$ ./6_new_str_print..py 
+#preferably only one way unless 
+# $ 
+#File: 6_what_needed.py
+
+#Now make sure you can answer these questions to complete this chapter
+#Why Python is awesome
+
+#How to use the Python interpreter
+
+#How to print text and variables using print
+
+#How to use strings
+
+#What are indexing and slicing in Python
+
+#What is the official Python coding style and how to check your code with pycodestyle
