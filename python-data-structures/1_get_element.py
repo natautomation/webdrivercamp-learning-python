7eeedac343bc33f5b844920776487100a3ac681e
natautomation <147779652+natautomation@users.noreply.github.com>
2023-10-22 17:14:51 -0700

    Create 1_get_element.py

---
+++ python-data-structures/1_get_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_element(lic,ind):
+   
+   if ind<0 or ind >=len(lic):
+      return None
+   else
+      print(f'The element get is{li[ind]}') 
+
+list_ = [5, 4, 3, 2, 1]
+index = 2
+print_element(list_, index)
