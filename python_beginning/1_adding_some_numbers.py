d2eb834de0ee853468a4578c2dec9598ddcb72f8
natautomation <147779652+natautomation@users.noreply.github.com>
2023-10-22 20:10:29 -0700

    Create 1_adding_some_numbers.py

---
+++ python_beginning/1_adding_some_numbers.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+level = 100
+print(f"Learning Python is fun\"' - {level} %")
+print("Learning Python is fun\"' - {} %".format(level))
+print("Learning Python is fun\"' - %s" % level, "%")
