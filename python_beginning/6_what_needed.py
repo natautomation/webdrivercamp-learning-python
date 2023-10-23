3f2281198fa827de011e99d0767546c9a76a204a
natautomation <147779652+natautomation@users.noreply.github.com>
2023-10-22 20:16:36 -0700

    Create 6_what_needed.py

---
+++ python_beginning/6_what_needed.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+my_var = "There should be one-- and preferably only one --obvious way to do it. Athough that way may not be obvious at first unless you're Dutch"
+split_var = my_var.split()
+result_var = split_var[5] + ' ' + split_var[6] + ' ' + split_var[7] + ' ' + split_var[9] + ' ' + split_var[22]
+print(result_var)
